7ec74b8897682f57cf1abeae02094f339e304cc1
M <m@Ms-MacBook-Air.local>
2022-10-28 22:40:16 +0000

    new class views added fixed logic in html files

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.contrib import admin
 from . import views
 from .views import (
+    ArticleDetail,
     HomeView,
     Login,
     OurStory,
@@ -9,9 +10,13 @@ from .views import (
     Logout,
     ArticleCreateView,
     EditArticle,
-    WrittenStories
+    Tagged,
+    WrittenStories,
+    DeleteView,
+    ArticleDetail,
     )
-#login_required
+
+
 from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path(
@@ -21,29 +26,53 @@ urlpatterns = [
         ),
     path(
         "create_article",
-        ArticleCreateView.as_view(template_name = "web/create_article.html"),
+        login_required(
+            ArticleCreateView.as_view(template_name = "web/create_article.html")
+            ),
         name="create_article"
         ),
-    path("tag/<slug:slug>/", views.tagged, name="tagged"),
-    path("edit/<int:pk>/", login_required(EditArticle.as_view(template_name = "web/update_article.html")), name="edit_article"),
-    path('article_detail/<int:pk>/', views.article_detail, name='article_detail'),
+    path(
+        "tag/<slug:slug>/",
+        Tagged.as_view(template_name = "web/home.html" ),
+        name="tagged"
+        ),
+    path(
+        "edit/<int:pk>/",
+        login_required(
+            EditArticle.as_view(template_name = "web/update_article.html")
+            ),
+         name="edit_article"
+         ),
+    path(
+        'article_detail/<int:pk>/',
+        login_required(
+            ArticleDetail.as_view(template_name = "web/article_detail_view.html")
+        ),
+        name='article_detail'
+        ),
     path('like/<int:pk>', views.like, name='like'),
     path('follow/<int:pk>/', views.follow, name='follow'),
-    path('delete', views.delete, name='delete'),
+    path(
+        'delete/<int:pk>/',
+        login_required(DeleteView.as_view()),
+            name='delete'
+            ),
     path('profile', views.profile, name='profile'),
     path('edit_profile', views.edit_user_info, name='edit_user_info'),
     path('create_user_info', views.create_user_info, name='create_user_info'),
     path('about', OurStory.as_view(), name='our_story'),
     path(
         'written',
-        login_required(WrittenStories.as_view(template_name = "web/written_stories.html")),
+        login_required(
+            WrittenStories.as_view(template_name = "web/written_stories.html")
+            ),
         name='written_stories'
         ),
 
     path(
         'register',
         RegisterView.as_view(template_name='web/register.html'),
-        name='class_register_view',
+        name='register',
         ),
     path(
         'login',
